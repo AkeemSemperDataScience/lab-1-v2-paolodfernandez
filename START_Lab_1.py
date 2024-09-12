3b6b1cf74b936536b1bb128dc2f9be2a5d299554
Paolo <paologabriel.fernandez@gmail.com>
2024-09-11 21:23:05 -0600

    test test

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -17,7 +17,7 @@ def lab1Question2(name):
         elif is_odd % 2 == 0:
             print("False")
     else: print("None")
-lab1Question2()
+lab1Question2("Name")
 
 def lab1Question3(input_string, input_number):
     # Take in two inputs - a string and a number
