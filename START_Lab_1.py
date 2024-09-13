125ce3ce0615af6061fa89a2d1d7199500ae018c
Paolo <paologabriel.fernandez@gmail.com>
2024-09-12 19:03:43 -0600

    test5

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -19,7 +19,9 @@ def lab1Question2(name):
 def lab1Question3(input_string, input_number):
     # Take in two inputs - a string and a number
     # Return the character of the string in the index given by number.  If this index does not exist, then return -1.
-    character_at = input_string[input_number]
+    if 0 <= input_number < len(input_string):
+        character_at = input_string[input_number]
+    else: character_at = -1
     return character_at
 
 def lab1Question4(file_name):
