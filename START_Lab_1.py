4ee964113a5841618470f0af916c2cc2d2e082f4
Paolo <paologabriel.fernandez@gmail.com>
2024-09-12 19:53:40 -0600

    test10

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -8,13 +8,12 @@ def lab1Question1(input_gb):
 lab1Question1(1)
 
 def lab1Question2(name):
-    is_odd = None
-    length = len(name)
-    if length % 2 == 1:
-        is_odd = True
-    else: is_odd = False
-
-    return is_odd
+    if isinstance(name, str):
+        if (len(name)) % 2 == 1:
+            print(True)
+        else: print(False)
+    else: print(None)
+lab1Question2()
 
 def lab1Question3(input_string, input_number):
     # Take in two inputs - a string and a number
